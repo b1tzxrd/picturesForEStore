e285517aa15d4cc609d181a717e1e437c86d94c5
b1tzxrd <bitzxrdcoding@gmail.com>
2025-02-24 15:30:54 +0500

    first commit

---
+++ assets/ReactEstore_files/main.tsx
@@ -0,0 +1,25 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=06243bcb"; const _jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=06243bcb"; const StrictMode = __vite__cjsImport1_react["StrictMode"];
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=06243bcb"; const createRoot = __vite__cjsImport2_reactDom_client["createRoot"];
+import { Provider } from "/src/components/ui/provider.tsx";
+import "/src/index.css";
+import App from "/src/App.tsx?t=1738826011891";
+createRoot(document.getElementById('root')).render(/*#__PURE__*/ _jsxDEV(StrictMode, {
+    children: /*#__PURE__*/ _jsxDEV(Provider, {
+        children: /*#__PURE__*/ _jsxDEV(App, {}, void 0, false, {
+            fileName: "/Users/bitzxrd./web/ReactLearn/ReactEStore/src/main.tsx",
+            lineNumber: 10,
+            columnNumber: 7
+        }, this)
+    }, void 0, false, {
+        fileName: "/Users/bitzxrd./web/ReactLearn/ReactEStore/src/main.tsx",
+        lineNumber: 9,
+        columnNumber: 5
+    }, this)
+}, void 0, false, {
+    fileName: "/Users/bitzxrd./web/ReactLearn/ReactEStore/src/main.tsx",
+    lineNumber: 8,
+    columnNumber: 3
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4udHN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IFN0cmljdE1vZGUgfSBmcm9tICdyZWFjdCdcbmltcG9ydCB7IGNyZWF0ZVJvb3QgfSBmcm9tICdyZWFjdC1kb20vY2xpZW50J1xuaW1wb3J0IHsgUHJvdmlkZXIgfSBmcm9tICcuL2NvbXBvbmVudHMvdWkvcHJvdmlkZXIudHN4J1xuaW1wb3J0ICcuL2luZGV4LmNzcydcbmltcG9ydCBBcHAgZnJvbSAnLi9BcHAudHN4J1xuXG5jcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdyb290JykhKS5yZW5kZXIoXG4gIDxTdHJpY3RNb2RlPlxuICAgIDxQcm92aWRlcj5cbiAgICAgIDxBcHAgLz5cbiAgICA8L1Byb3ZpZGVyPlxuICA8L1N0cmljdE1vZGU+LFxuKVxuIl0sIm5hbWVzIjpbIlN0cmljdE1vZGUiLCJjcmVhdGVSb290IiwiUHJvdmlkZXIiLCJBcHAiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwicmVuZGVyIl0sIm1hcHBpbmdzIjoiO0FBQUEsU0FBU0EsVUFBVSxRQUFRLFFBQU87QUFDbEMsU0FBU0MsVUFBVSxRQUFRLG1CQUFrQjtBQUM3QyxTQUFTQyxRQUFRLFFBQVEsK0JBQThCO0FBQ3ZELE9BQU8sY0FBYTtBQUNwQixPQUFPQyxTQUFTLFlBQVc7QUFFM0JGLFdBQVdHLFNBQVNDLGNBQWMsQ0FBQyxTQUFVQyxNQUFNLGVBQ2pELFFBQUNOO2NBQ0MsY0FBQSxRQUFDRTtrQkFDQyxjQUFBLFFBQUNDIn0=
